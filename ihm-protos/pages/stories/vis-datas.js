5e97e900d4b908b3d73c56c707344a71dd5cc78f
jguidoux <jeremie.guidoux@gmaid.com>
2016-05-26 20:04:37 +0200

    amériorate graph part in gaulois stories. tiltle ure all the weidth page and resume part show title and date

--- ihm-protos/pages/stories/vis-datas.js
+++ ihm-protos/pages/stories/vis-datas.js
@@ -18,11 +18,11 @@ var timeline = new vis.Timeline(container, items, options);
 
 
 var resumeMap = {};
-resumeMap[1] = "L’apparition des Celtes est généralement située dans le courant du IIe millénaire av. J.-C. (protohistoire).";
-resumeMap[2] = "Le Hallstatt ou Premier âge du fer est une période succédant à l'âge du bronze final et précédant la période de La Tène ou Second âge du fer (fin de la Protohistoire). Il tire son nom de celui d'un site archéologique qui se trouve à Hallstatt dans le Salzkammergut en Autriche.";
-resumeMap[3] = "L’expression Celtes historiques désigne les peuples directement liés au second âge du fer, la civilisation de La Tène (Ve siècle av. J.-C.) et particulièrement à l’expansion en direction de l’Italie et du Danube.";
-resumeMap[4] = "La guerre des Gaules, de 58 à 51 av. J.-C., est la guerre de conquête entreprise par le général romain Jules César et la résistance que lui opposent les tribus gauloises. La victoire romaine est assurée en 52 av. J.-C. par la bataille et le siège d'Alésia, où les Gaulois, dirigés par Vercingétorix, sont vaincus. La guerre cesse en 51 av. J.-C. après la prise de l'oppidum d'Uxellodunum (dans le Lot) où résistait encore la tribu des Cadurques. Le récit détaillé de cette guerre a été fait par Jules César dans ses Commentaires sur la Guerre des Gaules (De Bello Gallico).";
-resumeMap[5] = "En 27, Auguste organise administrativement la gaule en fondant 3 provinces gallo-romaines";
+resumeMap[1] = "<h2>Apparition du peuple celtes (-2000 av jC)</h2>L’apparition des Celtes est généralement située dans le courant du IIe millénaire av. J.-C. (protohistoire).";
+resumeMap[2] = "<h2>Civilisation de Hallstatt (-1300 -> -25 av JC)</h2>Le Hallstatt ou Premier âge du fer est une période succédant à l'âge du bronze final et précédant la période de La Tène ou Second âge du fer (fin de la Protohistoire). Il tire son nom de celui d'un site archéologique qui se trouve à Hallstatt dans le Salzkammergut en Autriche.";
+resumeMap[3] = "<h2>Les Celtes historiques (-450 -> -25 av JC)</h2>L’expression Celtes historiques désigne les peuples directement liés au second âge du fer, la civilisation de La Tène (Ve siècle av. J.-C.) et particulièrement à l’expansion en direction de l’Italie et du Danube.";
+resumeMap[4] = "<h2>La guerre des gaules (-58 -> -50 av JC)</h2>La guerre des Gaules, de 58 à 51 av. J.-C., est la guerre de conquête entreprise par le général romain Jules César et la résistance que lui opposent les tribus gauloises. La victoire romaine est assurée en 52 av. J.-C. par la bataille et le siège d'Alésia, où les Gaulois, dirigés par Vercingétorix, sont vaincus. La guerre cesse en 51 av. J.-C. après la prise de l'oppidum d'Uxellodunum (dans le Lot) où résistait encore la tribu des Cadurques. Le récit détaillé de cette guerre a été fait par Jules César dans ses Commentaires sur la Guerre des Gaules (De Bello Gallico).";
+resumeMap[5] = "<h2>Création des provinces gallo-romaines (40 ap JC)</h2>En 27, Auguste organise administrativement la gaule en fondant 3 provinces gallo-romaines";
 
 timeline.on('select', function (properties) {
     var id = properties["items"];
